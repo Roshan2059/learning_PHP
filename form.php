d656d01db7383de2934f5613e13ab411d29eb33b
Roshan <roshanpanta2059@gmail.com>
2022-04-01 22:26:58 +0545

    last commit of the day

---
+++ form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="form.php" method="POST">
+        <label>Name:</label>
+        <input type="text" name="name"><br>
+        <label>Email:</label>
+        <input type="email" name="mail"><br>
+        <label>Username:</label>
+        <input type="text" id="uname" name="uname"><br>
+        <label>Password</label>
+        <input type="password" name="pass"><br>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+
+</html>
